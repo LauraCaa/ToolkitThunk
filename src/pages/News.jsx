93108f1a6251ba42f23668abea432f52d7e11d24
Camila <cardenascastrolauracamila7@gmail.com>
2025-07-22 17:14:31 -0500

    feat: News page created & styles applied

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,35 @@
-import react from "react";
 import styles from "../styles/News.module.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "../components/Card";
+import { getNews } from "../features/newsSlice";
+import { clearNewsList } from "../features/newsSlice";
 
 const News = () => {
+  const {newsList, loading, error} = useSelector(state => state.news);
+  const dispatch = useDispatch(); 
+
+  useEffect(() => {
+    dispatch(getNews())
+    return() => {
+      dispatch(clearNewsList());
+    };
+  }, [dispatch]);
+
   return(
-    <div>News</div>
+    <div className={styles.newsWrapper}>
+      <h1>News</h1>
+      {error && <h1>{ error }</h1>}
+      {loading && <h1>Loading...</h1>}
+      {!loading && (
+        <div className={styles.cardWrapper}>
+          {newsList.map((item, index) => (
+            <Card key={index} {...item}/>
+          ))}
+        </div>
+
+      )}
+    </div>
   )
 };
 
