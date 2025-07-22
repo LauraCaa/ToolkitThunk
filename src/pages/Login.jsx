fe5ff18f5b9c693cad9baa578ba8e60fc349dcd1
Camila <cardenascastrolauracamila7@gmail.com>
2025-07-22 17:08:12 -0500

    feat: Updated form states

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,26 @@
-import {useState} from "react";
 import styles from "../styles/Login.module.css";
+import { useState } from "react";
 import { useNavigate} from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../features/authSlice";
 
 const Login = () => {
   const[email, setEmail] = useState("");
   const[password, setPassword] = useState("");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(setUser({ email, password }));
     navigate("/");
     setEmail("");
     setPassword("");
   };
+
   return(
     <div className={styles.formWrapper}>
-      <form className={styles.form}onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.inputWrapper}>
           <label htmlFor="email">Email</label>
           <input 
@@ -36,7 +42,6 @@ const Login = () => {
           />
         </div>
         <input type="submit" value="submit"/>
-        
       </form>
     </div>
   )
