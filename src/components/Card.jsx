974f450baaf7262f81d3f36d0ebbb665d59f3a98
Camila <cardenascastrolauracamila7@gmail.com>
2025-07-22 17:15:06 -0500

    feat: Card component created & styles applied

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,23 @@
 import styles from "../styles/Card.module.css";
 
-const Card = () => {
+const Card = ({title, url, urlToImage, content}) => {
   return(
-    <div>Card</div>
+    <div className={styles.card}>
+      <img src={urlToImage} alt="news"/>
+      <div className={styles.cardDetail}>
+        <h3>{title}</h3>
+        <p>{content}</p>
+      </div>
+      <div className={styles.a}> 
+          <a 
+            href={url} 
+            rel="noreferrer" 
+            target="_blank" 
+            className={styles.link} 
+          > Detail
+          </a>
+        </div>
+    </div>
   )
 };
 
