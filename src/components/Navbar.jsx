af37f4a6b72ea42e636fe262239f7d473c9d5be1
Camila <cardenascastrolauracamila7@gmail.com>
2025-07-22 10:47:47 -0500

    feat: Created & styled Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const user = true;
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
   return(
-    <div>Navbar</div>
+    <div className={styles.navbarWrapper}>
+      <div>
+        <h1>OAL NEWS</h1>
+        <p> {user} </p>
+      </div>
+      <div className={styles.buttonWrapper}>
+        {user && <button onClick={handleLogout}>Logout</button>}
+        {!user && <button onClick={() => navigate("/login")}>Login</button>}
+      </div>
+    </div>
   )
 };
 
